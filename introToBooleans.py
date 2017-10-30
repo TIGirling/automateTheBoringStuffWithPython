5f15d4ef7347281d1151eaa13f5f5a43dae6972c
Tim Girling <TIGirling@googlemail.com>
2017-10-30 14:04:20 +0000

    Add elif statements to show flow control concepts

--- introToBooleans.py
+++ introToBooleans.py
@@ -1,8 +1,17 @@
 name = input("What is your name? ")
 password = input("What is your password? ")
+age = int(input("What is your age? "))
 
 if name == "Mary":
     print("Hello Mary")
+elif age < 30:
+    print("You are not Mary, you child.")
+elif age > 2000:
+    print("You are not Mary. You are the great undead!")
+elif age > 50:
+    print("Your old ass aint Mary!")
+else:
+    print("Hello stranger")
     if password == "swordfish":
         print("Access Granted.")
     else:
