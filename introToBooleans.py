b1e0f176068bbd31236a8ed56647d49eb61166e0
Tim Girling <TIGirling@googlemail.com>
2017-10-30 13:42:31 +0000

    add a file to for a beginner intro to booleans

---
+++ introToBooleans.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+password = input("What is your password? ")
+
+if name == "Mary":
+    print("Hello Mary")
+    if password == "swordfish":
+        print("Access Granted.")
+    else:
+        print("Wrong Password")
